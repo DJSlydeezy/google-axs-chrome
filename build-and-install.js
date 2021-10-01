cca56caae47667cb9efe702974801e7e6ccf9b12
Mr.Phixit <mr.phixit59@gmail.com>
2021-10-01 15:16:22 -0700

    Create build and install.js

---
+++ build-and-install.js
@@ -0,0 +1,6 @@
+// See the License for the specific language governing permissions and
+// limitations under the License.
+goog.require('cvox.ApiUtil');
+goog.require('cvox.AriaUtil');
+goog.require('cvox.BuildInfo');
+goog.require('cvox.ChromeVox');
